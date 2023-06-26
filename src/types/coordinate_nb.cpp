a45eb1491ca384b75d682ac8042147dc76b4f73f
whytro <whytro@users.noreply.github.com>
2023-06-26 16:00:56 +0900

    Remove FloatCoordinate Bindings

--- src/types/coordinate_nb.cpp
+++ src/types/coordinate_nb.cpp
@@ -13,28 +13,10 @@ void init_Coordinate(nb::module_& m) {
     using FloatLatitude = osrm::Alias<double, tag::latitude>;
     
     using osrm::util::Coordinate;
-    using osrm::util::FloatCoordinate;
-
-    nb::class_<FloatCoordinate>(m, "FloatCoordinate")
-        .def(nb::init<>())
-        .def(nb::init<const Coordinate>(), "other"_a)
-        .def("__init__", [](FloatCoordinate* t, std::pair<double, double> coords) {
-            const FloatLongitude lon_ = FloatLongitude{coords.first};
-            const FloatLatitude lat_ = FloatLatitude{coords.second};
-
-            new (t) FloatCoordinate(lon_, lat_);
-         })
-        .def_rw("lon", &FloatCoordinate::lon)
-        .def_rw("lat", &FloatCoordinate::lat)
-        .def("IsValid", &FloatCoordinate::IsValid)
-        .def(nb::self == nb::self)
-        .def(nb::self != nb::self);
-    nb::implicitly_convertible<std::pair<double, double>, FloatCoordinate>();
 
     nb::class_<Coordinate>(m, "Coordinate")
         .def(nb::init<>())
         .def(nb::init<const Coordinate&>(),"coordinate"_a)
-        .def(nb::init<const FloatCoordinate&>(), "other"_a)
         .def("__init__", [](Coordinate* t, std::pair<double, double> coords) {
             const FloatLongitude lon_ = FloatLongitude{coords.first};
             const FloatLatitude lat_ = FloatLatitude{coords.second};
