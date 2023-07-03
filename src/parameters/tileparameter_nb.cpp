af1adc7a74325475eaf2e68fb7764262ec184fc5
whytro <whytro@users.noreply.github.com>
2023-07-03 18:39:48 +0900

    Add validity check for TileParameter argument

--- src/parameters/tileparameter_nb.cpp
+++ src/parameters/tileparameter_nb.cpp
@@ -6,6 +6,8 @@
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
 
+#include <stdexcept>
+
 namespace nb = nanobind;
 using namespace nb::literals;
 
@@ -14,15 +16,19 @@ void init_TileParameters(nb::module_& m) {
 
     nb::class_<TileParameters>(m, "TileParameters", nb::is_final())
         .def(nb::init<>())
-        .def(nb::init<unsigned, unsigned, unsigned>(),
+        .def(nb::init<unsigned int, unsigned int, unsigned int>(),
             "x"_a, "y"_a, "z"_a
         )
-        .def("__init__", [](TileParameters* t, const std::vector<unsigned>& coord) {
+        .def("__init__", [](TileParameters* t, const std::vector<unsigned int>& coord) {
+            if(coord.size() != 3) {
+                throw std::runtime_error("Parameter must be an array [x, y, z]");
+            }
+            
             new (t) TileParameters{coord[0], coord[1], coord[2]};
          })
         .def_rw("x", &TileParameters::x)
         .def_rw("y", &TileParameters::y)
         .def_rw("z", &TileParameters::z)
         .def("IsValid", &TileParameters::IsValid);
-    nb::implicitly_convertible<std::vector<unsigned>, TileParameters>();
+    nb::implicitly_convertible<std::vector<unsigned int>, TileParameters>();
 }
