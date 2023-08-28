2c7f48ed2d87761d9a6a0118f0f2fa0e8d408941
Whytro <whytro@users.noreply.github.com>
2023-08-28 20:50:39 +0900

    Fix executable calling from __main__ (#25)

--- src/osrm/__main__.py
+++ src/osrm/__main__.py
@@ -6,35 +6,33 @@ if(len(sys.argv) < 2):
     print("Argument not provided")
     sys.exit(1)
 
-execpath = site.getusersitepackages()+"/bin/"
+exec = site.getusersitepackages()+"/bin/"
 
 if(sys.argv[1] == "components"):
-    execpath += "osrm-components"
+    exec += "osrm-components"
 
 elif(sys.argv[1] == "contract"):
-    execpath += "osrm-contract"
+    exec += "osrm-contract"
 
 elif(sys.argv[1] == "customize"):
-    execpath += "osrm-customize"
+    exec += "osrm-customize"
 
 elif(sys.argv[1] == "datastore"):
-    execpath += "osrm-datastore"
+    exec += "osrm-datastore"
 
 elif(sys.argv[1] == "extract"):
-    execpath += "osrm-extract"
+    exec += "osrm-extract"
 
 elif(sys.argv[1] == "partition"):
-    execpath += "osrm-partition"
+    exec += "osrm-partition"
 
 elif(sys.argv[1] == "routed"):
-    execpath += "osrm-routed"
-
-args = [execpath]
+    exec += "osrm-routed"
 
 for i in range(2, len(sys.argv)):
-    args.append(sys.argv[i])
+    exec += " " + sys.argv[i]
 
 # will stream any output to the shell
 # shell=True is safe here and lets people use "~" in paths etc
-proc = subprocess.run(args, encoding="utf-8", capture_output=True, shell=True)
+proc = subprocess.run(exec, encoding="utf-8", shell=True)
 sys.exit(proc.returncode)
